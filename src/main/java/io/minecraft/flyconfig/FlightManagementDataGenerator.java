a88849301c7479717faf1c7a46f6b93d32fec6ef
ziyit-Hacker <3950140506@qq.com>
2026-02-05 09:02:43 +0800

    Initial commit

---
+++ src/main/java/io/minecraft/flyconfig/FlightManagementDataGenerator.java
@@ -0,0 +1,11 @@
+package io.minecraft.flyconfig;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FlightManagementDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/io/minecraft/flyconfig/item/ModItems.java
@@ -0,0 +1,41 @@
+package io.minecraft.flyconfig.item;
+
+import io.minecraft.flyconfig.FlightManagement;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.DeathProtectionComponent;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+
+import static io.minecraft.flyconfig.FlightManagement.MOD_ID;
+
+public class ModItems {
+    public static final Item METAL_SCRAP = registerItems("metal_scrap", new Item(new Item.Settings()
+            .maxCount(99).food(new FoodComponent.Builder().saturationModifier(0.3F).alwaysEdible().build()).rarity(Rarity.COMMON)));
+    public static final Item AIRCRAFT_OIL = registerItems("aircraft_oil", new Item(new Item.Settings()
+            .maxCount(16).rarity(Rarity.UNCOMMON)));
+    public static final Item FLIGHT_MANAGER = registerItems("flight_manager", new Item(new Item.Settings()
+            .maxCount(1).rarity(Rarity.EPIC).component(DataComponentTypes.DEATH_PROTECTION, DeathProtectionComponent.TOTEM_OF_UNDYING)));
+
+    private static Item registerItems(String id, Item item) {
+        // return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(MOD_ID, id)), item);
+        return Registry.register(Registries.ITEM, Identifier.of(MOD_ID, id), item);
+    }
+
+    private  static void addItemToIG(FabricItemGroupEntries fabricItemGroupEntries){
+        fabricItemGroupEntries.add(METAL_SCRAP);
+        fabricItemGroupEntries.add(AIRCRAFT_OIL);
+        fabricItemGroupEntries.add(FLIGHT_MANAGER);
+    }
+
+    public static void registerModItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemToIG);
+        FlightManagement.LOGGER.info("Registering ModItems.");
+    }
+}
