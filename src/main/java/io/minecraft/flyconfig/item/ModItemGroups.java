c5c6ea1b28d57de195c4d909854ff113264e2026
ziyit-Hacker <3950140506@qq.com>
2026-02-05 13:41:59 +0800

    Update Groups

--- src/main/java/io/minecraft/flyconfig/item/ModItemGroups.java
+++ src/main/java/io/minecraft/flyconfig/item/ModItemGroups.java
@@ -29,5 +29,6 @@ public class ModItemGroups {
                         }).build());
 
         FlightManagement.LOGGER.info("Registering Item Groups for " + FlightManagement.MOD_ID);
+        // 我是注释
     }
 }
