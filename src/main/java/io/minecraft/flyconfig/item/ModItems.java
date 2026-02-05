d8a4c7294942c5ef911f2ed7d986ab3f740d2aed
ziyit-Hacker <3950140506@qq.com>
2026-02-05 15:18:58 +0800

    Fix RegisterItems

--- src/main/java/io/minecraft/flyconfig/item/ModItems.java
+++ src/main/java/io/minecraft/flyconfig/item/ModItems.java
@@ -17,21 +17,20 @@ public class ModItems {
     private ModItems() {
     }
 
-    public static final Item METAL_SCRAP = registerItems("metal_scrap", Item::new, new Item(new Item.Settings()
-            .maxCount(99).food(new FoodComponent.Builder().saturationModifier(0.3F).alwaysEdible().build()).rarity(Rarity.COMMON)));
+    public static final Item METAL_SCRAP = registerItems("metal_scrap", Item::new, new Item.Settings()
+            .maxCount(99).food(new FoodComponent.Builder().saturationModifier(0.3F).alwaysEdible().build()).rarity(Rarity.COMMON));
 
-    public static final Item AIRCRAFT_OIL = registerItems("aircraft_oil", Item::new, new Item(new Item.Settings()
-            .maxCount(16).rarity(Rarity.UNCOMMON)));
-    public static final Item FLIGHT_MANAGER = registerItems("flight_manager", Item::new, new Item(new Item.Settings()
-            .maxCount(1).rarity(Rarity.EPIC).component(DataComponentTypes.DEATH_PROTECTION, DeathProtectionComponent.TOTEM_OF_UNDYING)));
+    public static final Item AIRCRAFT_OIL = registerItems("aircraft_oil", Item::new, new Item.Settings()
+            .maxCount(16).rarity(Rarity.UNCOMMON));
+    public static final Item FLIGHT_MANAGER = registerItems("flight_manager", Item::new, new Item.Settings()
+            .maxCount(1).rarity(Rarity.EPIC).component(DataComponentTypes.DEATH_PROTECTION, DeathProtectionComponent.TOTEM_OF_UNDYING));
 
     public static Item registerItems(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
         final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("tutorial", path));
         return Items.register(registryKey, factory, settings);
     }
 
-    private static Item registerItems(String metalScrap, Function<Item.Settings, Item> aNew, Item item) {
-        return item;
+    public static void initialize() {
     }
 
     public static void registerModItems() {
