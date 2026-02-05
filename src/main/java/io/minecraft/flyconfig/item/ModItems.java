ae026f70e24394950b17299b6be63505d0e26dfb
ziyit-Hacker <3950140506@qq.com>
2026-02-05 15:16:43 +0800

    Fix RegisterItems

--- src/main/java/io/minecraft/flyconfig/item/ModItems.java
+++ src/main/java/io/minecraft/flyconfig/item/ModItems.java
@@ -1,29 +1,37 @@
 package io.minecraft.flyconfig.item;
 
 import io.minecraft.flyconfig.FlightManagement;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.DeathProtectionComponent;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 
+import java.util.function.Function;
+
 public class ModItems {
-    public static final Item METAL_SCRAP = registerItems("metal_scrap", new Item(new Item.Settings()
+    private ModItems() {
+    }
+
+    public static final Item METAL_SCRAP = registerItems("metal_scrap", Item::new, new Item(new Item.Settings()
             .maxCount(99).food(new FoodComponent.Builder().saturationModifier(0.3F).alwaysEdible().build()).rarity(Rarity.COMMON)));
-    public static final Item AIRCRAFT_OIL = registerItems("aircraft_oil", new Item(new Item.Settings()
+
+    public static final Item AIRCRAFT_OIL = registerItems("aircraft_oil", Item::new, new Item(new Item.Settings()
             .maxCount(16).rarity(Rarity.UNCOMMON)));
-    public static final Item FLIGHT_MANAGER = registerItems("flight_manager", new Item(new Item.Settings()
+    public static final Item FLIGHT_MANAGER = registerItems("flight_manager", Item::new, new Item(new Item.Settings()
             .maxCount(1).rarity(Rarity.EPIC).component(DataComponentTypes.DEATH_PROTECTION, DeathProtectionComponent.TOTEM_OF_UNDYING)));
 
-    private static Item registerItems(String id, Item item) {
-        // return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(MOD_ID, id)), item);
-        return Registry.register(Registries.ITEM, Identifier.of("flyconfig", id), item);
+    public static Item registerItems(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("tutorial", path));
+        return Items.register(registryKey, factory, settings);
+    }
+
+    private static Item registerItems(String metalScrap, Function<Item.Settings, Item> aNew, Item item) {
+        return item;
     }
 
     public static void registerModItems() {
