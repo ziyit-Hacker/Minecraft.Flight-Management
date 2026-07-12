d1f72eb3fe15e92a68d36ccb13bf11d760e9e7ab
ziyit-Hacker <3950140506@qq.com>
2026-07-12 21:25:51 +0800

    Fix 合成配方 Add 非法方块的合成配方 Add 部分成就

--- src/main/java/io/minecraft/flyconfig/FlightManagement.java
+++ src/main/java/io/minecraft/flyconfig/FlightManagement.java
@@ -12,16 +12,11 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.GenerationStep;
 import org.slf4j.Logger;
